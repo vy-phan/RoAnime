887f539c24a74c34a5ffabd04a9f8094e3cb9ff4
vy-phan <phanluvy14102005@gmail.com>
2026-03-20 16:54:08 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import SearchPage from "./pages/SearchPage";
 import Footer from "./components/common/Footer";
 import TheatricalPage from "./pages/TheatricalPage";
 import HistoryPage from "./pages/HistoryPage";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
 
@@ -40,7 +39,6 @@ function App() {
         <Footer />
         <ScrollToTop />
       </BrowserRouter>
-      <SpeedInsights />
     </div>
   )
 }
